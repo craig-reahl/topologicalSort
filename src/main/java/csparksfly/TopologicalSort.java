cac5315ffdea4f135907a7b5700da395b32f9761
csparksfly <craig@reahl.org>
2019-09-01 08:51:57 +0200

    initial working

---
+++ src/main/java/csparksfly/TopologicalSort.java
@@ -0,0 +1,34 @@
+package csparksfly;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DirectedAcyclicGraph;
+import org.jgrapht.traverse.TopologicalOrderIterator;
+
+import java.util.Iterator;
+
+public class TopologicalSort {
+
+    public static void main(String[] args) {
+        Graph<String, DefaultEdge> graph = new DirectedAcyclicGraph<>(DefaultEdge.class);
+
+        graph.addVertex("root");
+        graph.addVertex("a");
+        graph.addVertex("b");
+        graph.addVertex("c");
+        graph.addVertex("d");
+
+        graph.addEdge("root", "a");
+        graph.addEdge("a", "b");
+        graph.addEdge("b", "c");
+        graph.addEdge("c", "d");
+        graph.addEdge("b", "d");
+
+        Iterator<String> iterator = new TopologicalOrderIterator<String, DefaultEdge>(graph);
+        while (iterator.hasNext()) {
+            String  s = iterator.next();
+            System.out.println(s);
+        }
+
+    }
+}
