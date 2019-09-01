48970a6433e44901434fb8c16817ef116edb61d4
csparksfly <craig@reahl.org>
2019-09-01 14:34:57 +0200

    export a graph to graphviz dot file

---
+++ src/main/java/csparksfly/GraphExport.java
@@ -0,0 +1,66 @@
+package csparksfly;
+
+import csparksfly.gradle.Dependency;
+import csparksfly.gradle.DependencyBuilder;
+import csparksfly.gradle.DependencyGraph;
+import csparksfly.gradle.DependencyTopologicalSort;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.io.ComponentNameProvider;
+import org.jgrapht.io.DOTExporter;
+import org.jgrapht.io.ExportException;
+import org.jgrapht.io.GraphExporter;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class GraphExport {
+
+    public static void Dot(Graph<String, DefaultEdge> graph, String filename) throws ExportException, IOException {
+
+
+        // adhering to the DOT language restrictions
+        ComponentNameProvider<String> vertexIdProvider = new ComponentNameProvider<String>()
+        {
+            public String getName(String name)
+            {
+                //return name.replace('.', '_').replace(":","_");
+                return ""+name.hashCode();
+            }
+        };
+        ComponentNameProvider<String> vertexLabelProvider = new ComponentNameProvider<String>()
+        {
+            public String getName(String name)
+            {
+                return name;
+            }
+        };
+        GraphExporter<String, DefaultEdge> exporter =
+                new DOTExporter<>(vertexIdProvider, vertexLabelProvider, null);
+        Writer writer = new StringWriter();
+        exporter.exportGraph(graph, writer);
+
+        Path path = Paths.get(filename);
+        byte[] strToBytes = writer.toString().getBytes();
+
+        Files.write(path, strToBytes);
+
+        System.out.println(writer.toString());
+    }
+
+    public static void main(String[] args) throws Exception{
+
+        List<Dependency> dependencies = DependencyBuilder.FromFile("src/test/resources/deps2.txt");
+
+        Graph<String, DefaultEdge> graph = DependencyGraph.Create(dependencies);
+
+        GraphExport.Dot(graph, "src/test/resources/sample.dot" );
+        //dot -Tpng src/test/resources/sample.dot  -o src/test/resources/sample.png
+
+    }
+}
--- src/main/java/csparksfly/gradle/DependencyTopologicalSort.java
+++ src/main/java/csparksfly/gradle/DependencyTopologicalSort.java
@@ -1,5 +1,6 @@
 package csparksfly.gradle;
 
+import csparksfly.GraphExport;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DirectedAcyclicGraph;
@@ -8,19 +9,15 @@ import org.jgrapht.traverse.TopologicalOrderIterator;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DependencyTopologicalSort {
 
 
     public static List<String> Show(List<Dependency> dependencies){
 
-        Graph<String, DefaultEdge> graph = new DirectedAcyclicGraph<>(DefaultEdge.class);
-
-        for (Dependency dep: dependencies) {
-            graph.addVertex(dep.getFrom().getName());
-            graph.addVertex(dep.getTo().getName());
-            graph.addEdge(dep.getFrom().getName(), dep.getTo().getName());
-        }
+        Graph<String, DefaultEdge> graph = DependencyGraph.Create(dependencies);
 
         List<String> sorted = new LinkedList<>();
         Iterator<String> iterator = new TopologicalOrderIterator<>(graph);
@@ -34,10 +31,18 @@ public class DependencyTopologicalSort {
 
     public static void main(String[] args) throws Exception {
 
-        List<Dependency> dependencies = DependencyBuilder.FromFile("src/test/resources/deps.txt");
+        List<Dependency> dependencies = DependencyBuilder.FromFile("src/test/resources/deps3.txt");
 
         List<String> sorted = DependencyTopologicalSort.Show(dependencies);
         for(String s : sorted) {
+            System.out.println(s);
+        }
+
+        List<String> froms = dependencies.stream().map(d->d.getFrom().getName()).collect(Collectors.toList());
+        Set<String> tos = dependencies.stream().map(d->d.getTo().getName()).filter(d->!froms.contains(d)).collect(Collectors.toSet());
+
+        System.out.println("\n\nNo Deps:");
+        for (String s : tos){
             System.out.println(s);
         }
 
